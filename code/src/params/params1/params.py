906701ee3157e01a572984083a734cd055322edf
eileenrmartin <ermartin@stanford.edu>
2017-04-27 12:45:27 -0700

    updated params for test data

--- code/src/params/params1/params.py
+++ code/src/params/params1/params.py
@@ -1,13 +1,13 @@
 import datetime as dt
 
-startTime = dt.datetime(2016,9,4,5,0,54,932*1000)
+startTime = dt.datetime(2016,9,12,13,0,54,932*1000)
 secondsPerFile = 60
 secondsPerWindowWidth = 300
 secondsPerWindowOffset = 150
 xCorrMaxTimeLagSeconds = 3.0
-nFiles = 240
+nFiles = 9
 outfilePath = '/scratch/ermartin/oneBitXCorr/'
-outfileListFile = '/home/ermartin/StanfordDASArray/inter_results/outfileList1.txt'
+outfileListFile = '/home/CorrelationCode/src/inter_results/outfileList1.txt'
 srcChList = 'sourceList.txt'
 startCh = 15
 endCh = 300
---
+++ code/src/params/params1plot/params.py
@@ -0,0 +1,16 @@
+import datetime as dt
+
+startTime = dt.datetime(2016,9,4,5,0,54,932*1000)
+secondsPerFile = 60
+secondsPerWindowWidth = 300
+secondsPerWindowOffset = 150
+xCorrMaxTimeLagSeconds = 3.0
+nFiles = 240*6
+outfilePath = '/scratch/'
+outfileListFiles = ['/home/CorrelationCode/src/inter_results/outfileList1.txt']
+srcChList = 'sourceList.txt'
+startCh = 15
+endCh = 300
+minFrq = 0.2
+maxFrq = 24.0
+recLocationsFilename = '/home/CorrelationCode/src/inter_results/passive_channel_UTM_coords.txt'
