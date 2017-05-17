653cd5c14bd460d3a3d9a68eb6f0ed4095822adb
fantine <fantine.huot@gmail.com>
2017-05-16 23:48:51 -0700

    Moved the data processing part to another file

--- code/src/training.py
+++ code/src/training.py
@@ -1,154 +1,58 @@
-#cimport numpy as np
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime as dt
 import struct
-import obspy
 import sys
-from reader import *
-import cwt
-from sklearn.preprocessing import scale
+import time
+from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering as AggloCluster
 
-# get parameters: startTime, secondsPerFile, secondsPerWindowWidth, secondsPerWindowOffset, xCorrMaxTimeLagSeconds, nFiles, outfilePath, outfileList, srcChList, startCh, endCh, minFrq, maxFrq
 paramsPath = sys.argv[1]
 sys.path.append(paramsPath)
 from params import *
-import fileSet as fs
-import regularFileSet as rfs
-
-# naming convention for input files
-parts=['/data/biondo/DAS/','year4','/','month','/','day','/cbt_processed_','year4','month','day','_','hour24start0','minute','second','.','millisecond','+0000.sgy']
 
 srcFile = open(srcChList,'r')
 srcChannelsStrings = srcFile.readlines()
 srcFile.close()
-srcChannels = [int(ch) for ch in srcChannelsStrings] 
-nChannels = endCh-startCh+1 # number of receiver channels
-
-# starting time and file set organization
-startYr = startTime.year
-startMo = startTime.month
-startDay = startTime.day
-startHr = startTime.hour
-startMin = startTime.minute
-startSec = startTime.second
-startMil = int(0.001*startTime.microsecond)
-regFileSet = rfs.regularFileSet(parts,startYr,startMo,startDay,startHr,startMin,startSec,startMil,secondsPerFile,nFiles)
-endTime = startTime + dt.timedelta(seconds=secondsPerFile*nFiles-1) # last time in last file
-fileList = regFileSet.getFileNamesInRange(startTime,endTime)
-# read a little header info about sample rate and number of channels
-import os
-nBytesPerFile = os.path.getsize(fileList[0])
-nChannelsTotal = 620
-bytesPerChannel = (nBytesPerFile-nTxtFileHeader-nBinFileHeader-240*nChannelsTotal)/nChannelsTotal
-samplesPerChannel = bytesPerChannel/4
-samplesPerSecond = samplesPerChannel/secondsPerFile 
-print("samples per second "+str(samplesPerSecond))
-NyquistFrq = float(samplesPerSecond)/2.0
-samplesPerFile = samplesPerSecond*secondsPerFile
-
-# figure out each window size
-samplesPerWindow = secondsPerWindowWidth*samplesPerSecond
-windowOffset = dt.timedelta(seconds=secondsPerWindowOffset)
-windowLength = dt.timedelta(seconds=secondsPerWindowWidth)
-currentWindowStartTime = startTime
-currentWindowEndTime = currentWindowStartTime + windowLength
-
+nChannels = endCh - startCh + 1
 outfileList = []
 
-count = 0 
-# these are 4 hour windows, there are 7 * 6 within one week and we downsample them over 0.5 sec (25 samples)
-nSamples = secondsPerWindowOffset * samplesPerSecond
+ndays = 7
+outfileListFile = outfileListFile[:-5]
+files = []
+nf = 30
+sps = 50
 samplingRate = 25
-nSubSamples = int(nSamples / samplingRate)
-nf = 25
-trainingData = np.empty((nChannels, nSubSamples * 7 * 6, nf * 2))
+nSamples = secondsPerWindowOffset * int(sps / samplingRate)
 
-# for each time window, compute cwt scales and build training data set
-while currentWindowEndTime < endTime:
-  print(currentWindowStartTime)
+for day in range(1, ndays + 1):
+  fileListName = outfileListFile + str(day) + '.txt'
+  f = open(fileListName,'r')
+  fileList = f.readlines()
+  dayFiles = [(file.strip('\n')) for file in fileList]
+  files.append(dayFiles)
+  f.close()
 
-  thisWindowsFileSet= regFileSet.getFileNamesInRange(currentWindowStartTime,currentWindowEndTime)
+nfiles = len(files)
 
-  data = np.zeros((nChannels,samplesPerWindow))
-  startIdx = 0
-  for filename in thisWindowsFileSet: 
-    thisFileStartTime = regFileSet.getTimeFromFilename(filename)
-    thisFileEndTime = thisFileStartTime + dt.timedelta(seconds=secondsPerFile)
-    startIdxReading = 0 # start index to read in filename
-    if currentWindowStartTime > thisFileStartTime:
-      secondsAfterStart = (currentWindowStartTime - thisFileStartTime).total_seconds()
-      startIdxReading = int(samplesPerSecond*secondsAfterStart)
-    endIdxReading = samplesPerFile
-    if currentWindowEndTime < thisFileEndTime:
-      secondsAfterStart = (currentWindowEndTime - thisFileStartTime).total_seconds()
-      endIdxReading = int(samplesPerSecond*secondsAfterStart)
-    nIdxToRead = endIdxReading-startIdxReading
-    endIdx = startIdx + nIdxToRead # end of this subset of data in array
-    # read the data
-    for ch in range(startCh,endCh+1):
-      data[ch-startCh,startIdx:endIdx] =  readTrace(filename,samplesPerFile,4,ch,'>',startIdxReading,nIdxToRead)
-    startIdx = endIdx # index in data array
-          
-  # take time derivatives
-  dataRate = data[:,1:] - data[:,:-1]
+trainingData = np.empty((nChannels, nSamples * nfiles, nf * 2), dtype=np.float64)
 
-  # do bandpass from minFrq to maxFrq
-  for ch in range(startCh,endCh+1):
-    thisTrace = obspy.core.trace.Trace(data=dataRate[ch-startCh,:],header={'delta':1.0/float(samplesPerSecond),'sampling_rate':samplesPerSecond})
-    thisTrace.filter('bandpass',freqmin=minFrq,freqmax=maxFrq,corners=4,zerophase=True)
-    dataRate[ch-startCh,:] = thisTrace.data
-
-  # get rid of laser drift, then do one bit simplification
-  dataRate = dataRate - np.median(dataRate, axis=0)
-
-  # compute cwt scales
-  delta = 1.0 / float(samplesPerSecond)
-  # compute cwt over time
-  f = np.logspace(np.log10(minFrq), np.log10(maxFrq), nf)
-  wf = 'morlet'
-  w0 = 8
-  scales = cwt.scales_from_fourier(f, wf, w0)
-  cwtScales = np.empty((nChannels, samplesPerWindow - 1, nf * 2))
-  for index, trace in enumerate(dataRate):
-    cwtScales[index,:,:nf] = np.abs(cwt.cwt(trace, delta, scales, wf, w0).T)
-  # compute cwt over space
-  minSpaceFrq = 0.5
-  maxSpaceFrq = 50 
-  f = np.logspace(np.log10(minSpaceFrq), np.log10(maxSpaceFrq), nf)
-  scales = cwt.scales_from_fourier(f, wf, w0)
-  for index, channel in enumerate(dataRate.T):
-    cwtScales[:,index,nf:] = np.abs(cwt.cwt(channel, delta, scales, wf, w0).T)
-
-  # cut off boundaries 
-  start = 60 * samplesPerSecond
-  end = start + nSamples
-  cwtScales = cwtScales[:,start:end,:]
-
-  # save the cwt scales
-  outfileName = outfilePath + 'cwt_' + str(currentWindowStartTime + dt.timedelta(seconds=60))
-  outfileName = outfileName.replace(" ","_") # don't have a space in the middle of the name
-  np.savez(outfileName, cwtScales)
-  outfileList.append(outfileName)
-
-  # subsample to 0.5 second windows
-  trainingData[:, (count * nSubSamples):((count + 1) * nSubSamples),:] = np.mean(np.reshape(cwtScales, (nChannels, nSubSamples, samplingRate, nf * 2)), axis=2)
-  count = count + 1
-
-  # move on to the next time step
-  currentWindowStartTime = currentWindowStartTime + windowOffset
-  currentWindowEndTime = currentWindowStartTime + windowLength
-
-# save the training data
-outfileName = outfilePath + 'trainingData'
-np.savez(outfileName, trainingData)
-outfileList.append(outfileName)
+outfilePathtmp = '/data/biondo/fantine/das/scratch2/'
+for index, file in enumerate(files):
+  file = outfilePathtmp + file
+  trainingData[:,(index * nSamples):((index + 1) * nSamples),:] = np.load(file)
 
 # Clustering
-trainingData = scale(np.reshape(trainingData, (nChannels * nSubSamples, nf * 2)), copy=False)
+trainingData = np.reshape(trainingData, (nChannels * nSamples * nfiles, -1))
+scaler = StandardScaler(copy=False).fit(trainingData)
+trainingData = scaler.transform(trainingData)
 nClusters = 4
+
+# dump the scaler
+from sklearn.externals import joblib
+outfileName = outfilePath + 'scaler.pkl'
+joblib.dump(scaler, outfileName)
+outfileList.append(outfileName)
 
 # K-means
 kmeans = KMeans(init='k-means++', n_clusters=nClusters, n_jobs=-1, n_init=10)
@@ -162,7 +66,6 @@ np.savez(outfileName, kmeans.cluster_centers_)
 outfileList.append(outfileName)
 
 # dump the estimator
-from sklearn.externals import joblib
 outfileName = outfilePath + 'kmeans.pkl'
 joblib.dump(kmeans, outfileName)
 outfileList.append(outfileName)
@@ -180,3 +83,9 @@ outfileName = outfilePath + 'aggloCluster.pkl'
 joblib.dump(aggloCluster, outfileName)
 outfileList.append(outfileName)
 
+# write the list of output file names
+outfileListFile = outfileListFile + '_clustering'
+outFile = open(outfileListFile,'w')
+for filename in outfileList:
+  outFile.write(filename + '\n')
+outFile.close()
